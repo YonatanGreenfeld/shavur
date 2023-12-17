eb9bd3559b6a0889358d6d82788d36683879fb6c
henkling <105640757+henkling@users.noreply.github.com>
2023-12-17 14:40:04 +0200

    Update ff.tf

--- ff.tf
+++ ff.tf
@@ -1,26 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-resource "aws_s3_bucket" "positive2" {
-  bucket = "my-tf-test-bucket"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+  bucket = "my-s3-bucket"
   acl    = "public-read-write"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-  versioning {
+  versioning = {
     enabled = true
   }
 }
